794df6a0a264464e31081761770d4a7c65e04970
Claudio-Ardura <Claudio-Ardura>
2013-04-28 18:31:31 +0200

    Los usuarios y ventas se devuelven en una lista porque no se modifican

--- src/main/java/es/uniovi/asw/entrecine/interfaces/AdministrationToDatabase.java
+++ src/main/java/es/uniovi/asw/entrecine/interfaces/AdministrationToDatabase.java
@@ -1,12 +1,17 @@
 package es.uniovi.asw.entrecine.interfaces;
 
+import java.util.List;
+
 import javax.sql.rowset.CachedRowSet;
+
+import es.uniovi.asw.entrecine.model.Sale;
+import es.uniovi.asw.entrecine.model.User;
 
 public interface AdministrationToDatabase {
 	
-	CachedRowSet getUsers();
+	List<User> getUsers();
 	
-	CachedRowSet getSales();
+	List<Sale> getSales();
 	
 	CachedRowSet getBillboard();
 	
